e94caceae3776fc15cfccfbbe1e9e4e79cc7ca73
njugunamwangi <njugunamwangi.n@gmail.com>
2023-08-08 16:06:03 +0300

    implement layouts

---
+++ react/src/router.jsx
@@ -0,0 +1,44 @@
+import {createBrowserRouter, Navigate} from "react-router-dom";
+import Dashboard from "./views/Dashboard.jsx";
+import Login from "./views/Login.jsx";
+import Surveys from "./views/Surveys.jsx";
+import Signup from "./views/Signup.jsx";
+import GuestLayout from "./components/GuestLayout.jsx";
+import DefaultLayout from "./components/DefaultLayout.jsx";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <DefaultLayout />,
+        children: [
+            {
+                path: '/dashboard',
+                element: <Navigate to="/" />
+            },
+            {
+                path: '/',
+                element: <Dashboard />
+            },
+            {
+                path: 'surveys',
+                element: <Surveys />
+            },
+        ]
+    },
+    {
+        path: '/',
+        element: <GuestLayout />,
+        children: [
+            {
+                path: 'login',
+                element: <Login />
+            },
+            {
+                path: 'signup',
+                element: <Signup />
+            },
+        ]
+    }
+])
+
+export default router;
