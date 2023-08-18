0a8dff87254a02e65e613d02ae7d6eff9cffdb70
njugunamwangi <njugunamwangi.n@gmail.com>
2023-08-19 01:21:35 +0300

    List surveys from database

--- react/src/views/Surveys.jsx
+++ react/src/views/Surveys.jsx
@@ -1,14 +1,27 @@
 import PageComponent from "../components/PageComponent.jsx";
-import {useStateContext} from "../context/ContextProvider.jsx";
 import SurveyListItem from "../components/SurveyListItem.jsx";
 import TButton from "../components/core/TButton.jsx";
 import {PlusCircleIcon} from "@heroicons/react/20/solid/index.js";
+import {useEffect, useState} from "react";
+import axiosClient from "../axios.js";
+import PaginationLinks from "../components/PaginationLinks.jsx";
 
 export default function Surveys() {
-    const { surveys } = useStateContext();
+    const [surveys, setSurveys] = useState([]);
+
+    const [loading, setLoading] = useState(false)
     const onDeleteClick = () => {
         console.log("On delete click");
     }
+
+    useEffect(() => {
+        setLoading(true)
+        axiosClient.get('/survey')
+            .then(({data}) => {
+                setSurveys(data.data)
+                setLoading(false)
+            })
+    }, [])
 
     return (
         <PageComponent title="Surveys" buttons={(
@@ -16,11 +29,18 @@ export default function Surveys() {
                 <PlusCircleIcon className="h-6 w-6 mr-2" />Create New
             </TButton>
         )}>
-            <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3">
-                {surveys.map(survey => (
-                    <SurveyListItem survey={survey} key={survey.id} onDeleteClick={onDeleteClick} />
-                ))}
-            </div>
+            {loading && (<div>
+                Loading...
+            </div>)}
+            {!loading && (<div>
+                <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3">
+                    {surveys.map(survey => (
+                        <SurveyListItem survey={survey} key={survey.id} onDeleteClick={onDeleteClick}/>
+                    ))}
+                </div>
+
+                <PaginationLinks/>
+            </div>)}
         </PageComponent>
     )
 }
