4ef3f10de2ea4736fd84ee6217572547bdf0d37b
njugunamwangi <njugunamwangi.n@gmail.com>
2023-08-20 17:40:41 +0300

    implement pagination

--- react/src/views/Surveys.jsx
+++ react/src/views/Surveys.jsx
@@ -1,47 +1,65 @@
-import PageComponent from "../components/PageComponent.jsx";
-import SurveyListItem from "../components/SurveyListItem.jsx";
-import TButton from "../components/core/TButton.jsx";
-import {PlusCircleIcon} from "@heroicons/react/20/solid/index.js";
-import {useEffect, useState} from "react";
-import axiosClient from "../axios.js";
-import PaginationLinks from "../components/PaginationLinks.jsx";
-import {BlocksWave} from "react-svg-spinners";
+import { PlusCircleIcon } from "@heroicons/react/24/outline";
+import { useEffect, useState } from "react";
+import axiosClient from "../axios";
+import TButton from "../components/core/TButton";
+import PageComponent from "../components/PageComponent";
+import PaginationLinks from "../components/PaginationLinks";
+import SurveyListItem from "../components/SurveyListItem";
+import {TwelveDotsScaleRotate} from "react-svg-spinners";
 
 export default function Surveys() {
     const [surveys, setSurveys] = useState([]);
+    const [meta, setMeta] = useState({});
+    const [loading, setLoading] = useState(false);
 
-    const [loading, setLoading] = useState(false)
-    const onDeleteClick = () => {
-        console.log("On delete click");
-    }
+    const onPageClick = (link) => {
+        getSurveys(link.url);
+    };
+
+    const getSurveys = (url) => {
+        url = url || "/survey";
+        setLoading(true);
+        axiosClient.get(url).then(({ data }) => {
+            setSurveys(data.data);
+            setMeta(data.meta);
+            setLoading(false);
+        });
+    };
 
     useEffect(() => {
-        setLoading(true)
-        axiosClient.get('/survey')
-            .then(({data}) => {
-                setSurveys(data.data)
-                setLoading(false)
-            })
-    }, [])
+        getSurveys();
+    }, []);
 
     return (
-        <PageComponent title="Surveys" buttons={(
-            <TButton color="green" to="/surveys/create">
-                <PlusCircleIcon className="h-6 w-6 mr-2" />Create New
-            </TButton>
-        )}>
-            {loading && (<div className="items-center">
-                <BlocksWave  />
-            </div>)}
-            {!loading && (<div>
-                <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3">
-                    {surveys.map(survey => (
-                        <SurveyListItem survey={survey} key={survey.id} onDeleteClick={onDeleteClick}/>
-                    ))}
+        <PageComponent
+            title="Surveys"
+            buttons={
+                <TButton color="green" to="/surveys/create">
+                    <PlusCircleIcon className="h-6 w-6 mr-2" />
+                    Create new
+                </TButton>
+            }
+        >
+            {loading && <div className="text-center text-lg"> <TwelveDotsScaleRotate /> </div>}
+            {!loading && (
+                <div>
+                    {surveys.length === 0 && (
+                        <div className="py-8 text-center text-gray-700">
+                            You don't have surveys created
+                        </div>
+                    )}
+                    <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 md:grid-cols-3">
+                        {surveys.map((survey) => (
+                            <SurveyListItem
+                                survey={survey}
+                                key={survey.id}
+                                onDeleteClick={onDeleteClick}
+                            />
+                        ))}
+                    </div>
+                    {surveys.length > 0 && <PaginationLinks meta={meta} onPageClick={onPageClick} />}
                 </div>
-
-                <PaginationLinks/>
-            </div>)}
+            )}
         </PageComponent>
-    )
+    );
 }
