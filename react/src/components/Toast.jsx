362da0797a4750a89a9c652e4051fe47bfd43736
njugunamwangi <njugunamwangi.n@gmail.com>
2023-08-21 04:25:36 +0300

    Bug fixes

--- react/src/components/Toast.jsx
+++ react/src/components/Toast.jsx
@@ -7,7 +7,7 @@ export default function Toast() {
         <>
             {
                 toast.show && (
-                    <div className="w-[300px] py-2 px-3 text-white rounded bg-emarald-500 fixed right-2 bottom-2 z-50 animate-fade-in-down">
+                    <div className="w-[300px] py-2 px-3 text-white rounded bg-emerald-500 fixed right-2 bottom-2 z-50 animate-fade-in-down">
                         {toast.message}
                     </div>
                 )
