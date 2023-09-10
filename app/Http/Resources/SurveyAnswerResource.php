61a0179929b6ecf9de196f3ed54f203297b0f2ab
njugunamwangi <njugunamwangi.n@gmail.com>
2023-09-10 11:18:39 +0300

    Resource logic

--- app/Http/Resources/SurveyAnswerResource.php
+++ app/Http/Resources/SurveyAnswerResource.php
@@ -14,6 +14,10 @@ class SurveyAnswerResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'survey' => new SurveyResource($this->survey),
+            'end_date' => $this->end_date
+        ];
     }
 }
--- app/Http/Resources/SurveyDashboardResource.php
+++ app/Http/Resources/SurveyDashboardResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\URL;
 
 class SurveyDashboardResource extends JsonResource
 {
@@ -14,6 +15,17 @@ class SurveyDashboardResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'slug' => $this->slug,
+            'image_url' => $this->image ? URL::to($this->image) : null,
+            'status' => !!$this->status,
+            'description' => $this->description,
+            'created_at' => $this->created_at->format('Y-m-d H:i:s'),
+            'expire_date' => (new \DateTime($this->expire_date))->format('Y-m-d'),
+            'questions' => $this->questions()->count(),
+            'answers' => $this->answers()->count()
+        ];
     }
 }
